3fa3f58043ee69d9ba9e08986f7e59e5ddec95eb
Maxim Bodrak <kilbirdih@gmail.com>
2016-03-20 16:30:19 +0500

    PatternSample

---
+++ Patterns/src/MyFactoryMethod/Golf.java
@@ -0,0 +1,29 @@
+package MyFactoryMethod;
+
+/**
+ * Created by Kilbirdih on 01.02.2016.
+ */
+public class Golf implements Car {
+    private String body;
+    private String name;
+    private String engine;
+    public Golf(String name, String body, String engine){
+        this.body = body;
+        this.name = name;
+        this.engine = engine;
+    }
+    @Override
+    public String getBody() {
+        return body;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getEngine() {
+        return engine;
+    }
+}
