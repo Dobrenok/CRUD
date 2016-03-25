de4b2065144ff7449155d834b4f3ac310e9ac8ca
Dobrenok <vladislav7496@mail.ru>
2016-03-26 00:19:45 +0500

    Delete CarMaker.java

--- Patterns/src/MyFactoryMethod/CarMaker.java
+++
@@ -1,9 +0,0 @@
-package MyFactoryMethod;
-
-/**
- * Created by Kilbirdih on 01.02.2016.
- */
-public interface CarMaker {
-    Car createCar(String name, String body, String engine);
-    int open = 7;
-}
